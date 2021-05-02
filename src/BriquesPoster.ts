008e4d6d401d00d7da800f451457c1dc6aa1fdae
Julien Lengrand-Lambert <julien@lengrand.fr>
2021-05-02 18:03:58 +0200

    Plus ou moins OK à gauche

--- src/BriquesPoster.ts
+++ src/BriquesPoster.ts
@@ -4,55 +4,110 @@ import {customElement, property} from 'lit/decorators.js';
 @customElement("briques-poster")
 export class BriquesPoster extends LitElement {
 
-  @property() title = 'Brique du Web';
+  @property() title = 'Briques du Web';
 
   static styles = css`
+
+    h2, h3, h4{
+      margin: 0;
+      padding: 0;
+    }
+
     :host {
+      font-size : 5rem;
+      font-family: Chawp;
+      color: white;
+
       display: inline-block;
       width: 1920px;
       height: 1080px;
       background-image: url("assets/brick5.jpeg");
       background-repeat: repeat;
       background-size: auto;
-
     }
 
     main {
-      flex-grow: 1;
+      display: flex;
+      width: 100%;
+      height:100%;
     }
 
+    .left-side{
+      flex: 0 0 40%;
+      display: flex;
+      flex-direction: column;
+    }
 
-    .app-footer {
-      font-size: calc(12px + 0.5vmin);
+    .right-side{
+      flex: 0 0 60%;
+
+      background-color : green;
+      opacity: 0.5;
+
+    }
+
+    .logo{
+      flex: 0 0 50%;
+      display: flex;
       align-items: center;
+      justify-content: center;
     }
+
+    .logo img{
+      width:80%;
+      object-fit:contain;
+    }
+
+    .whichwhen{
+      flex: 0 0 50%;
+      display: flex;
+      flex-direction: column;
+    }
+
+    .which{
+      flex: 0 0 30%;
+      display:flex;
+      align-items: center;
+      justify-content: center;
+
+    }
+
+    .when{
+      flex: 0 0 70%;
+    }
+
+    .when h3{
+      text-align: center
+    }
+
+    .when .space{
+      margin-top:5%;
+    }
+
   `;
 
   render() {
     return html`
       <main>
-        <h1>${this.title}</h1>
+        <div class="left-side">
+            <div class="logo">
+              <img src="assets/logo_color_text_square.png">
+            </div>
+            <div class="whichwhen">
+                <div class="which">
+                  <h2>S01E04</h2>
+                </div>
+                <div class="when">
+                    <h3 class="space">Mardi 27/04</h3>
+                    <h3>12h15</h3>
+                </div>
+            </div>
 
-        <p>Edit <code>src/BriquesPosters.ts</code> and save to reload.</p>
-        <a
-          class="app-link"
-          href="https://open-wc.org/guides/developing-components/code-examples"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Code examples
-        </a>
+        </div>
+        <div class="right-side">
+
+        </div>
       </main>
-
-      <p class="app-footer">
-        🚽 Made with love by
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://github.com/open-wc"
-          >open-wc</a
-        >.
-      </p>
     `;
   }
 }
