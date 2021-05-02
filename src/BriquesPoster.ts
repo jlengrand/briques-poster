af7b5fce1baf571aa51a34426291134ccdc074b0
Julien Lengrand-Lambert <julien@lengrand.fr>
2021-05-02 15:30:13 +0200

    Create repo and latest lit

---
+++ src/BriquesPoster.ts
@@ -0,0 +1,51 @@
+import {LitElement, html, css} from 'lit';
+import {customElement, property} from 'lit/decorators.js';
+
+@customElement("briques-poster")
+export class BriquesPoster extends LitElement {
+
+  @property() title = 'My app';
+
+  static styles = css`
+    :host {
+    }
+
+    main {
+      flex-grow: 1;
+    }
+
+
+    .app-footer {
+      font-size: calc(12px + 0.5vmin);
+      align-items: center;
+    }
+  `;
+
+  render() {
+    return html`
+      <main>
+        <h1>${this.title}</h1>
+
+        <p>Edit <code>src/BriquesPosters.ts</code> and save to reload.</p>
+        <a
+          class="app-link"
+          href="https://open-wc.org/guides/developing-components/code-examples"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Code examples
+        </a>
+      </main>
+
+      <p class="app-footer">
+        🚽 Made with love by
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://github.com/open-wc"
+          >open-wc</a
+        >.
+      </p>
+    `;
+  }
+}
---
+++ test/briques-posters.test.ts
@@ -0,0 +1,21 @@
+// import { html, fixture, expect } from '@open-wc/testing';
+//
+// import { BriquesPosters } from '../src/BriquesPosters.js';
+// import '../src/briques-posters.js';
+//
+// describe('BriquesPosters', () => {
+//   let element: BriquesPosters;
+//   beforeEach(async () => {
+//     element = await fixture(html`<briques-posters></briques-posters>`);
+//   });
+//
+//   it('renders a h1', () => {
+//     const h1 = element.shadowRoot!.querySelector('h1')!;
+//     expect(h1).to.exist;
+//     expect(h1.textContent).to.equal('My app');
+//   });
+//
+//   it('passes the a11y audit', async () => {
+//     await expect(element).shadowDom.to.be.accessible();
+//   });
+// });
